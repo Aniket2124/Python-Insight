5b51f78b3b8233165b210a1339ea3d20a2c6e9a1
Aniket2124 <patilaniket04@gmail.com>
2021-04-03 00:48:43 +0530

    removed unwanted code

--- for_loop.py
+++ for_loop.py
@@ -1,4 +1,4 @@
-'''
+
 word = " Hello"
 
 Letters = []
@@ -14,9 +14,8 @@ Numbers = [1, 2, 3, 4, 5]
 for n in Numbers:
     if n % 2 == 0:
         print(n)
-'''
+
 # range(start, stop, steps)
-'''
 
 
 # printing Odd Numbers
@@ -26,23 +25,3 @@ for num in range(1, 10, 2):
     Numbers.append(num)
     print(num)
 print(Numbers)
-'''
-
-
-def digit(start, n):
-    Numbers = []
-    for num in range(1, n+1):
-        if num % 3 == 0 and num % 5 == 0:
-            Numbers.append("FizzBuzz")
-        elif num % 3 == 0:
-            Numbers.append("Fizz")
-        elif num % 5 == 0:
-            Numbers.append("Buzz")
-        else:
-            Numbers.append(str(num))
-
-    return Numbers
-
-
-verify = digit(1, 100)
-print(verify)
