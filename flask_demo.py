de5b4e824c2bbccefe101ef103d3e64e15230405
Aniket2124 <patilaniket04@gmail.com>
2021-04-27 17:09:49 +0530

    Added code snippet- flask

---
+++ flask_demo.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return '{"name": "ani", "age": 29, "city": "York", "actor": "aaa"}'
+
+
+@app.route('/getJson')
+def hello_world2():
+    return '{"name": "abi", "age": 29, "city": "York", "actor": "aaa"}'
+
+
+@app.route('/connect_to_the_game')
+def hello_world3():
+    return '{"name": "abi", "age": 29, "city": "York", "actor": "aaa"}'
+
+
+if __name__ == '__main__':
+    app.run(host='192.168.0.104', port=4000)
